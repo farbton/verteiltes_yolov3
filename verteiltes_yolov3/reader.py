00ca90612577a664cd021535e3cdc2033c9957ac
farbton <farbton@gmx.net>
2020-06-23 23:50:54 +0200

    load video implementiert

--- verteiltes_yolov3/reader.py
+++ verteiltes_yolov3/reader.py
@@ -1,7 +1,7 @@
 #import numpy
 import os
 import cv2
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore 
 
@@ -12,7 +12,7 @@ class Reader(object):
 
     def load_image(self):
         print("load image ...")
-        self.mainWindow.statusBar().showMessage("load image")
+        self.mainWindow.statusBar().showMessage("load image ...")
         height = self.mainWindow.player.geometry().height()
         width = self.mainWindow.player.geometry().width()
         image_name = "images/12-12-2019 MONO 30fps 11_33_48_Kaefer auf Korn_4200mikros0.jpg"
@@ -26,6 +26,21 @@ class Reader(object):
 
     def load_video(self):
         print("load video ...")
-        self.mainWindow.statusBar().showMessage("load video")
-
-        #QtWidgets.QApplication.event()
+        self.mainWindow.statusBar().showMessage("play video ...")
+        videoName = "videos/YOLOv3_output_29.05.2020_10s.avi"
+        height = self.mainWindow.player.geometry().height()
+        width = self.mainWindow.player.geometry().width()
+        cap = cv2.VideoCapture(videoName)
+        while(cap.isOpened()):
+            ret,frame = cap.read()
+            if ret == False:
+                break
+            #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frameImage = QImage(frame.data, frame.shape[1], frame.shape[0], QImage.Format_RGB888)
+            pixMap = QPixmap.fromImage(frameImage)
+            pixMap = pixMap.scaled(QtCore.QSize(height, width), QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
+            scene = QtWidgets.QGraphicsScene()
+            scene.addPixmap(pixMap) # return pixmapitem
+            self.mainWindow.player.setScene(scene)
+            QtWidgets.QApplication.processEvents()
+        self.mainWindow.statusBar().clearMessage()
