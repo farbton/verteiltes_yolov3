c00cdd65cc91fabf3e609c0f5a8e8557a0c55682
farbton <farbton@gmx.net>
2020-07-04 19:35:25 +0200

    can load an image just autoscroll won't work

--- verteiltes_yolov3/mainwindow.py
+++ verteiltes_yolov3/mainwindow.py
@@ -1,5 +1,9 @@
 import sys
+import cv2
+import os
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5 import QtCore
 
 from reader import Reader
 from yolo import Yolo
@@ -12,7 +16,10 @@ class Window(QtWidgets.QMainWindow):
         uic.loadUi("gui.ui", self)
         self.setWindowTitle("Viewer for Yolov3")
         self.reader = Reader(self)
-        self.yolo = Yolo()
+        self.yolo = Yolo(self)
+        self.lock = True
+        self.scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        #self.scrollArea.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
 
     def start(self):        
         self.refresh_button.clicked.connect(self.label_write)
@@ -23,38 +30,80 @@ class Window(QtWidgets.QMainWindow):
     def startDetection(self):
         print("startDetection")
         self.statusBar().showMessage("detection ... ")
-        string = self.console.text() + "start detection ... \n"
-        self.console.setText(string)
+        self.console.setText(self.console.text() + "start detection ... " + str(self.scrollArea.verticalScrollBar().maximum()) + "\n")
         self.console.repaint()
-        self.repaint()
-        self.reader.loadYolofromDarknet()
+        #self.autoscroll()
+        self.detectImage()
+
+    def detectImage(self):
+        self.detectedImage = self.yolo.detectImage()
+        self.convertCv2ToQImage()
+        #cv2.imshow("test", self.detectedImage)
+        self.setPlayerHeightWidth()
+        self.qimageToPixmap()
+        self.resizePixmap()
+        self.pixmapSetScene()
+        self.addSceneToPlayer()
         self.statusBar().clearMessage()
         self.console.setText(self.console.text() + "ready ... \n")
+        self.console.repaint()
+        self.autoscroll()
+        self.lock = False
 
     def label_write(self):
         self.label.setText("hallo")
      
+    def setPlayerHeightWidth(self):
+        self.playerHeight = self.player.geometry().height()
+        self.playerWidth = self.player.geometry().width()
+
+    def convertCv2ToQImage(self):
+        self.qimage = QImage(self.detectedImage, 512, 512, QImage.Format_BGR888)
+
+    def qimageToPixmap(self):
+        self.pixMap = QPixmap(self.qimage)
+
+    def resizePixmap(self):
+        self.pixMap = self.pixMap.scaled(QtCore.QSize(self.playerHeight, self.playerWidth), QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
+    
+    def pixmapSetScene(self):
+        self.scene = QtWidgets.QGraphicsScene()
+        self.scene.addPixmap(self.pixMap) # return pixmapitem
+
+    def addSceneToPlayer(self):
+        self.player.setScene(self.scene)
+
     def loadImage(self):
         self.statusBar().showMessage("load image ...")
         self.statusBar().repaint()
-        height = self.player.geometry().height()
-        width = self.player.geometry().width()
-        #imageScene = self.reader.getImage()
-        #self.player.setScene(imageScene)
-        #self.statusBar().clearMessage()
+        self.setPlayerHeightWidth()
+        self.qimage = self.reader.getQImage()
+        self.autoscroll()
+        self.qimageToPixmap()
+        self.resizePixmap()
+        self.pixmapSetScene()
+        self.addSceneToPlayer()
+        self.lock = False
+        self.statusBar().clearMessage()
 
     def loadVideo(self):
         self.statusBar().showMessage("play video ...")
         videoScene = self.reader.getVideo()
         self.statusBar().clearMessage()
 
+    def autoscroll(self):
+        print("mainWindow.autoscroll()")
+        print("mainWindow...maximum(): ", self.scrollArea.verticalScrollBar().maximum())
+        self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().maximum())
+        #self.scrollArea.verticalScrollBar().setSliderDown(True)
+        #self.scrollArea.repaint()
+        #self.repaint()
+
     def resizeEvent(self, event):
-        print("Event")
-        #self.loadImage()
-
-    #def refresh_graphicView_image(self, pix_map_item):
-    #    scene = QtWidgets.QGraphicsScene()
-    #    scene.addItem(pix_map_item)
-    #    self.graphicsView.setScene(scene)
-    #    return
-
+        if self.lock == False:
+            print("Event")           
+            self.setPlayerHeightWidth()
+            self.qimageToPixmap()
+            self.resizePixmap()
+            self.pixmapSetScene()
+            self.addSceneToPlayer()
